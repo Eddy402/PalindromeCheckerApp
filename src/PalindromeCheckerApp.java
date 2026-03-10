f23e138de3240bbc977db061925bcef4ec8f71c1
Eddy402 <edsonthomas775@gmail.com>
2026-03-10 15:53:15 +0530

    UC5:Stack-Based Palindrome Checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,13 @@
+import java.util.Stack;
+
 /**
- * UseCase4PalindromeCheckerApp
+ * UseCase5PalindromeCheckerApp
  *
- * Demonstrates palindrome checking using a character array
- * and the two-pointer technique.
+ * Demonstrates palindrome checking using a Stack data structure.
+ * The stack reverses the characters using LIFO principle.
  *
  * @author Edson
- * @version 4.0
+ * @version 5.0
  */
 
 public class PalindromeCheckerApp {
@@ -13,30 +15,25 @@ public class PalindromeCheckerApp {
     public static void main(String[] args) {
 
         // Input string
-        String word = "radar";
+        String word = "madam";
 
-        // Convert string to character array
-        char[] characters = word.toCharArray();
+        // Create a stack
+        Stack<Character> stack = new Stack<>();
 
-        // Two-pointer approach
-        int start = 0;
-        int end = characters.length - 1;
-
-        boolean isPalindrome = true;
-
-        while (start < end) {
-
-            if (characters[start] != characters[end]) {
-                isPalindrome = false;
-                break;
-            }
-
-            start++;
-            end--;
+        // Push characters into the stack
+        for (int i = 0; i < word.length(); i++) {
+            stack.push(word.charAt(i));
         }
 
-        // Display result
-        if (isPalindrome) {
+        // Build reversed string by popping characters
+        String reversed = "";
+
+        while (!stack.isEmpty()) {
+            reversed = reversed + stack.pop();
+        }
+
+        // Compare original and reversed strings
+        if (word.equals(reversed)) {
             System.out.println("The string \"" + word + "\" is a Palindrome.");
         } else {
             System.out.println("The string \"" + word + "\" is NOT a Palindrome.");
